7b46eacede30d434937ded2f2429b3f6d8d6c10d
Ujjawal <ujjawal8461@gmail.com>
2026-01-11 16:24:14 +0530

    fix: added hash passwords using bcrypt and use in memory array

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,8 +1,18 @@
-const register = (req, res) => {
+const { users } = require("../utils/inMemoryUserStorage")
+const bcrypt = require("bcrypt")
+
+const register = async (req, res) => {
     const { email, password } = req.body;
 
     console.log("Rgister API Called");
-    console.log("Api req:", req.body, email, password);
+    // console.log("Api req:", req.body, email, password);
+
+    console.log(users);
+    if (users.find(u => u.email === email)) {
+        return res.status(409).json({
+            message: "User Already Exisit"
+        })
+    }
 
     if (!email || !password) {
         return res.status(400).json({
@@ -10,13 +20,21 @@ const register = (req, res) => {
         })
     }
 
+    const hashbPassword = await bcrypt.hash(password, 10)
+
+    users.push({ email, password: hashbPassword });
+    console.log(users);
+
     return res.status(201).json({
         message: "User Registered Succefully"
     })
 
 }
 
-const login = (req, res) => {
+
+
+
+const login = async (req, res) => {
     const { email, password } = req.body;
     console.log("login")
 
@@ -26,13 +44,16 @@ const login = (req, res) => {
         })
     }
 
-    if (email !== "rahul@gmail.com") {
+    const user = users.find(u => u.email === email)
+    if (!user) {
         return res.status(404).json({
             message: "User Not Found"
         })
     }
-    if (email === "rahul@gmail.com" && password !== "12345678") {
-        res.status(401).json({
+
+    const isMatch = await bcrypt.compare(password, user.password)
+    if (!isMatch) {
+        return res.status(401).json({
             message: "Password is wrong"
         })
     }
